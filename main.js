6ee549703477d7e07730bd3c345171ccd50fc7aa
shauryadandwani <116278582+shauryadandwani@users.noreply.github.com>
2023-09-09 13:26:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+
+if (error){
+    console.log(error);
+    
+} else {
+    random_number_r=Math.floor(Math.random()*255)+1;
+    random_number_g=Math.floor(Math.random()*255)+1;
+    random_number_b=Math.floor(Math.random()*255)+1;
+    
+    document.getElementById("result_label").innerHTML='i can hear - '+ results[0].label;
+    document.getElementById("result_confidence").innerHTML = 'Accuracy - '+ (results[0].confidence*100).toFixed(2)+" %";
+    document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+    document.getElementById("result_confidence").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+    
+    if (results[0].label=="cat") {
+img
+    }
+
+}
